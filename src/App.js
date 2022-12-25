c2b9c43b4af97b73847366a27ced8ebbcdce1602
Md Danish <85988020+Md-Danish-eng@users.noreply.github.com>
2022-12-25 14:55:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-//             Edit <code>src/App.js</code> and save to reload.
             Hurray! You have successfully deploy React-auth-app using AWS Amplify.
           </p>
           <a
